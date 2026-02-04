4a0394f43a869c70b43e96bd642a264b247e138a
fracispime <francisco.ponte.pimentel@student.nhlstenden.com>
2026-02-04 15:28:32 +0100

    file

---
+++ extensions/extensions.py
@@ -0,0 +1,18 @@
+filename = input("File name: ").lower().strip()
+
+if filename.endswith(".gif"):
+    print("image/gif")
+elif filename.endswith(".jpg") or filename.endswith(".jpeg"):
+    print("image/jpeg")
+elif filename.endswith(".png"):
+    print("image/png")
+elif filename.endswith(".pdf"):
+    print("application/pdf")
+elif filename.endswith(".txt"):
+    print("text/plain")
+elif filename.endswith(".zip"):
+    print("application/zip")
+elif filename.endswith(".html") or filename.endswith(".htm"):
+    print("text/html")
+else:
+    print("application/octet-stream")
