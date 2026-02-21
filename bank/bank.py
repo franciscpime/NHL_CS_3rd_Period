ccd5789b2dd14a261c9065f8183952ce56bc3972
fracispime <francisco.ponte.pimentel@student.nhlstenden.com>
2026-02-21 12:45:37 +0000

    Back to the Bank

--- bank/bank.py
+++ bank/bank.py
@@ -1,15 +1,15 @@
 def main():
-    greeting = input("Greeting: ").lower()
+    greeting = input("Greeting: ")
     print(value(greeting))
 
 
 def value(greeting):
-    if greeting.strip() == "hello" or greeting == "hello there" or greeting == "hello, newman":
-        return "$0"
-    elif greeting[0] == "h":
-        return "$20"
+    if greeting.strip().lower().startswith("hello"):
+        return 0
+    elif greeting.strip().lower().startswith("h"):
+        return 20
     else:
-        return "$100"
+        return 100
 
 if __name__ == "__main__":
     main()
--- bank/test_bank.py
+++ bank/test_bank.py
@@ -2,12 +2,16 @@ import bank
 from bank import value
 
 def test_specific():
-    assert value("hello") == "$0"
+    assert value("hello") == 0
 
 
 def test_start_h():
-    assert value("holla") == "$20"
+    assert value("holla") == 20
 
 
 def test_others():
-    assert value("Salut") == "$100"
+    assert value("Salut") == 100
+
+
+def test_complete_sentence():
+    assert value("Hello my friend") == 0
