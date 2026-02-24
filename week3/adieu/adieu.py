5173ff998a7b1346b0dbd35dfe5512f27ed16ac5
fracispime <francisco.ponte.pimentel@student.nhlstenden.com>
2026-02-24 19:55:15 +0000

    adieu

--- week3/adieu/adieu.py
+++ week3/adieu/adieu.py
@@ -4,12 +4,13 @@ while True:
     try:
         name = input("Name: ")
         names.append(name)
-
     except EOFError:
-        if len(names) == 1:
-            print(f"Adieu, adieu, to {names[0]}")
-        elif len(names) == 2:
-            print(f"Adieu, adieu, to {names[0]} and {names[1]}")
-        else:
-            print(f"Adieu, adieu, to {', '.join(names[:-1])}, and {names[-1]}")
+        print()  
         break
+
+if len(names) == 1:
+    print(f"Adieu, adieu, to {names[0]}")
+elif len(names) == 2:
+    print(f"Adieu, adieu, to {names[0]} and {names[1]}")
+elif len(names) >= 3:
+    print(f"Adieu, adieu, to {', '.join(names[:-1])}, and {names[-1]}")
